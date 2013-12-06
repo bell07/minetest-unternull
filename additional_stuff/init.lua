879ab112958d5e042891a0b653ae66a95c7a4b15
Casimir <jul.lutz@gmx.net>
2013-12-06 04:58:04 +0100

    support minetest_game

--- additional_stuff/init.lua
+++ additional_stuff/init.lua
@@ -21,3 +21,50 @@ minetest.register_craft({
 		{'default:papyrus_roots', 'default:papyrus_roots'},
 	}
 })
+
+if not minetest.get_modpath("nodetest") then
+
+minetest.register_node(":nodetest:papyrus_roots", {
+	description = "Papyrus Roots",
+	tiles = {"nodetest_papyrus_roots.png"},
+	paramtype = "light",
+	is_ground_content = true,
+	liquids_pointable = true,
+--	after_dig_node = function(pos, node, metadata, digger)
+--			node.name = "default:papyrus"
+--			default.dig_up(pos, node, digger)
+--		end,
+	groups = {snappy=3,flammable=2},
+	sounds = default.node_sound_leaves_defaults(),
+})
+
+minetest.register_craft({
+	output = 'nodetest:papyrus_roots',
+	recipe = {
+		{'default:papyrus', 'default:papyrus', 'default:papyrus'},
+		{'default:papyrus', 'default:papyrus', 'default:papyrus'},
+		{'default:papyrus', 'default:papyrus', 'default:papyrus'},
+	}
+})
+
+minetest.register_craft({
+	output = 'default:papyrus 9',
+	recipe = {
+		{'nodetest:papyrus_roots'},
+	}
+})
+
+minetest.register_abm({
+	nodenames = {"nodetest:papyrus_roots"},
+	neighbors = {"group:water"},
+	interval = 50,
+	chance = 50,
+	action = function(pos, node)
+		pos.y = pos.y+1
+		if minetest.get_node(pos).name == "air" then
+			minetest.set_node(pos, {name="default:papyrus"})
+		end
+	end,
+})
+
+end
--- give_initial_stuff/init.lua
+++ give_initial_stuff/init.lua
@@ -1,5 +1,5 @@
 minetest.register_on_newplayer(function(player)
-	player:get_inventory():add_item('main', 'default:papyrus_roots 9')
+	player:get_inventory():add_item('main', 'nodetest:papyrus_roots 9')
 	player:get_inventory():add_item('main', 'default:dirt 9')
 	player:get_inventory():add_item('main', 'default:cobble 9')
 	player:get_inventory():add_item('main', 'default:sapling')
