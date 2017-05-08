375ac905758e73f93eb44649ba523771b4f995d3
Alexander Weber <web.alexander@web.de>
2017-05-08 17:16:37 +0200

    desert and siver sand and stones in grinder and by recipes. Removed dirt from grinder

--- crushingfurnace/init.lua
+++ crushingfurnace/init.lua
@@ -25,7 +25,9 @@ local crushingfurnace_receipes = {
 	--input                 output            time
 	{"default:cobble", "default:gravel",      8},
 	{"default:gravel", "default:sand",        5},
-	{"default:sand", "default:dirt",        5},
+	{"default:sand", "default:clay_lump",     5},
+	{"default:glass", "default:silver_sand",   3},
+	{"default:desert_cobble", "default:desert_sand", 5},
 }
 
 function crushingfurnace_get_craft_result(input)
