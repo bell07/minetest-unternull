190e7ddb03e8fb828f95fbaa93e5c31a9785f438
Alexander Weber <web.alexander@web.de>
2017-05-09 23:02:45 +0200

    removed silver sand, added dirt again

--- crushingfurnace/init.lua
+++ crushingfurnace/init.lua
@@ -25,8 +25,8 @@ local crushingfurnace_receipes = {
 	--input                 output            time
 	{"default:cobble", "default:gravel",      8},
 	{"default:gravel", "default:sand",        5},
-	{"default:sand", "default:clay_lump",     5},
-	{"default:glass", "default:silver_sand",   3},
+	{"default:sand", "default:dirt",        5},
+	{"default:dirt", "default:clay_lump",     5},
 	{"default:desert_cobble", "default:desert_sand", 5},
 }
 
